c8ba76e84e232e3bc2dc45dfbeedba7b205c873c
mohitsolanki7051 <mohitsolanki7051@gmail.com>
2026-04-09 15:53:05 +0530

    Update download app

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -228,10 +228,10 @@
     <div class="sidebar-download">
         <p>Desktop App</p>
         <p>Required to activate tunnels</p>
-        <button class="download-btn">
+        <a href="https://github.com/mohitsolanki7051/tunara-app/releases/download/v1.0.0/Tunara.Setup.1.0.0.exe" class="download-btn">
             <svg fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"/></svg>
             Download for Windows
-        </button>
+        </a>
     </div>
 
     <div class="sidebar-user">
